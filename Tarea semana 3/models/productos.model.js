8e19fbd5a96fe7132b9661b1bb1a41ab330bb7c2
Tony-Toa86 <184334095+Tony-Toaf86@users.noreply.github.com>
2026-02-25 23:47:38 -0600

    Tarea semana 3 terminada

--- Tarea semana 3/models/productos.model.js
+++ Tarea semana 3/models/productos.model.js
@@ -1,22 +1,31 @@
 const db = require('../database')
 
-const Productos= {
+const Productos = {
     crear: (id, nombre, precio, proveedor_id, callback) => {
         const sql = 'INSERT INTO productos (id, nombre, precio, proveedor_id) VALUES (?, ?, ?, ?)';
         db.query(sql, [id, nombre, precio, proveedor_id], callback);
     },
 
     listar: (callback) => {
-        const sql = 'SELECT * FROM productos';
+        const sql = `
+        SELECT p.id, p.nombre, p.precio, p.proveedor_id, pr.nombre AS proveedor
+        FROM productos p
+        LEFT JOIN proveedores pr ON p.proveedor_id = pr.id
+    `;
         db.query(sql, callback);
     },
 
     obtnerPorId: (id, callback) => {
-        const sql = 'SELECT * FROM productos where id = ?';
+        const sql = `
+        SELECT p.id, p.nombre, p.precio, p.proveedor_id, pr.nombre AS proveedor
+        FROM productos p
+        LEFT JOIN proveedores pr ON p.proveedor_id = pr.id
+        WHERE p.id = ?
+    `;
         db.query(sql, [id], callback);
     },
-    
-    
+
+
     actualizar: (id, nombre, precio, proveedor_id, callback) => {
         const sql = 'UPDATE productos SET nombre = ?, precio = ?, proveedor_id = ? WHERE id = ?';
         db.query(sql, [nombre, precio, proveedor_id, id], callback);
@@ -24,7 +33,7 @@ const Productos= {
     eliminar: (id, callback) => {
         const sql = 'DELETE FROM productos WHERE id = ?'
         db.query(sql, [id], callback);
-    }      
+    }
 };
 
 module.exports = Productos;
